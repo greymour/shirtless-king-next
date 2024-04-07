c33307b9550d3a38723df1035ced7be3594860a9
Kurt Steigleder <kurtsteigleder@gmail.com>
2024-04-07 12:38:18 -0700

    Fix database_url constant accessing wrong env variable, add more routes to APP_URL constant and make API_URL as `as const`

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,14 @@
 export const API_URL = {
   LOGIN: '/api/login/',
-}
+} as const;
+
+export const APP_URL = {
+  HOME: '/',
+  INVENTORY: {
+    INVENTORY_ITEM: (id: string) => `/inventory/${id}/`,
+    ADD_ITEM: '/inventory/add/',
+  }
+} as const;
 
 export const DATABASE_AUTH_TOKEN = process.env.DATABASE_AUTH_TOKEN!
-export const DATABASE_URL = process.env.DATABASE_AUTH_TOKEN!
+export const DATABASE_URL = process.env.DATABASE_URL!
